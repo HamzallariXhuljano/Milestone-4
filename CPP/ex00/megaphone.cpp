439f6b9c10067e6b164b4fa97ee325d76083162c
Xhuljano Hamzallari <xhamzall@c2r2p6.42firenze.it>
2025-07-28 20:51:35 +0200

    ex00 done

--- CPP/ex00/megaphone.cpp
+++ CPP/ex00/megaphone.cpp
@@ -1,13 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.c                                        :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: xhamzall <xhamzall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 20:07:18 by xhamzall          #+#    #+#             */
-/*   Updated: 2025/07/28 20:08:51 by xhamzall         ###   ########.fr       */
+/*   Updated: 2025/07/28 20:50:45 by xhamzall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 
+int	main(int ac, char **av)
+{
+	int i = 1;
+
+	if (ac <= 1)
+		std :: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while (i < ac)
+	{
+		std :: cout << av[i];
+		i++;
+	}
+	 std :: cout << std :: endl;
+}
